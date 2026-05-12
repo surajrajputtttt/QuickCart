f9214b6a040da2a8b3753df85b55e477688e52bf
surajrajputtttt <surajkumar7061374@gmail.com>
2026-05-12 13:54:36 +0530

    fix: inngest functions

--- config/inngest.js
+++ config/inngest.js
@@ -3,7 +3,8 @@ import { Inngest } from "inngest";
 export const inngest = new Inngest({ id: "quickcart-next" });
 
 export const SyncUserCreation = inngest.createFunction(
-  { id: 'sync-user-from-clerk', event: 'clerk/user.created' },
+  { id: 'sync-user-from-clerk' },
+  { event: 'clerk/user.created' },
   async ({ event }) => {
     const connectDB = (await import("./db.js")).default;
     const user = (await import("../models/users.js")).default;
@@ -20,7 +21,8 @@ export const SyncUserCreation = inngest.createFunction(
 )
 
 export const syncuserUpdation = inngest.createFunction(
-  { id: 'update-user-from-clerk', event: 'clerk/user.updated' },
+  { id: 'update-user-from-clerk' },
+  { event: 'clerk/user.updated' },
   async ({ event }) => {
     const connectDB = (await import("./db.js")).default;
     const user = (await import("../models/users.js")).default;
@@ -37,29 +39,13 @@ export const syncuserUpdation = inngest.createFunction(
 )
 
 export const syncUserDeletion = inngest.createFunction(
-  { id: 'delete-user-with-clerk', event: 'clerk/user.deleted' },
+  { id: 'delete-user-with-clerk' },
+  { event: 'clerk/user.deleted' },
   async ({ event }) => {
     const connectDB = (await import("./db.js")).default;
     const user = (await import("../models/users.js")).default;
     const { id } = event.data
     await connectDB()
     await user.findByIdAndDelete(id)
-  }
-)
-export const syncUserCreation = inngest.createFunction(
-  { id: "sync-user-from-clerk" },        // 1st argument - config
-  { event: "clerk/user.created" },        // 2nd argument - trigger
-  async ({ event }) => {                  // 3rd argument - handler
-    const connectDB = (await import("./db.js")).default;
-    const user = (await import("../models/users.js")).default;
-    const { id, first_name, last_name, email_addresses, image_url } = event.data;
-    const userData = {
-      _id: id,
-      email: email_addresses[0].email_address,
-      name: first_name + ' ' + last_name,
-      image_url: image_url
-    }
-    await connectDB()
-    await user.create(userData)
   }
 )
